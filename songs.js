b8115646dc7c9e779ce8562c65608b7348a078e3
Sandy Jones <sandynashv@gmail.com>
2016-05-11 21:17:47 -0500

    added jshint to package.json

--- songs.js
+++ songs.js
@@ -1,10 +1,26 @@
 "use strict";
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
+
+
+$.ajax({
+  url: "songs.json",
+  success: loadFunction
 });
 
+$("#addMusic").click(function(){
+  $("#target").click();
+});
+
+
+$loadFunction = function(){
+  
+}
+
+
+
 // let expandedSongs = [];
-// let yourArray = [];	
+// let yourArray = [];  
 // let newSongsList = [];
 // let songs = [];
 
@@ -20,38 +36,39 @@ $( document ).ready(function() {
 // for (let i=0; i<songs.length; i++) {  
 //   expandedSongs = songs[i].replace(/[!*@(]/g, '').replace(/>/g, '');  
 // }
-// //gather song input from  user
-// let createNewMusic = function(){
-// 	// console.log("this function works", createNewMusic);
-// 	let songInput = document.getElementById("Song").value;
-// 	let artistInput = document.getElementById("Artist").value;
-// 	let albumInput = document.getElementById("Album").value;
-//   yourArray = [songInput, artistInput, albumInput];
-//   let newSongString = toString(yourArray);
-//   songs.push(newSongString);
-//   console.log("songs",songs);
-//   populateDOM();
-// }
-// function toString(arrayToConvert) {
-//   console.log("arrayToConvert", arrayToConvert);
-//   let songString = arrayToConvert[0] + " ";
-//   songString += arrayToConvert[1] + " ";
-//   songString += arrayToConvert[2];
-//   return songString;
-// }
+//gather song input from  user
+let createNewMusic = function(){
+  // console.log("this function works", createNewMusic);
+  let songInput = document.getElementById("Song").value;
+  let artistInput = document.getElementById("Artist").value;
+  let albumInput = document.getElementById("Album").value;
+  yourArray = [songInput, artistInput, albumInput];
+  let newSongString = toString(yourArray);
+  songs.push(newSongString);
+  console.log("songs",songs);
+  populateDOM();
+}
+function toString(arrayToConvert) {
+  console.log("arrayToConvert", arrayToConvert);
+  let songString = arrayToConvert[0] + " ";
+  songString += arrayToConvert[1] + " ";
+  songString += arrayToConvert[2];
+  return songString;
+}
 
-// //convert to jquery
+//convert to jquery
 
 
-// addBtn.addEventListener("click", createNewMusic); 
-// // populateDOM(newSongsList);
-// function populateDOM() {
-//   for (let i = 0; i < songs.length; i++) {
-//     let songs = songs[i];
-//     currentSongs.innerHTML += songs[i]; 
-//     console.log("songs[i]", songs[i]);  
-//     }
-// };
-// populateDOM(); 
-// createNewMusic(songs);    
-// addClickEvent();
+addBtn.addEventListener("click", createNewMusic); 
+// populateDOM(newSongsList);
+function populateDOM() {
+  for (let i = 0; i < songs.length; i++) {
+    let songs = songs[i];
+    currentSongs.innerHTML += songs[i]; 
+    console.log("songs[i]", songs[i]);  
+    }
+};
+populateDOM(); 
+createNewMusic(songs);    
+addClickEvent();
+});
