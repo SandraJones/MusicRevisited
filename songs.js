2dba9f2d3990e74587c5bac5491b51c12c75faa4
Sandy Jones <sandynashv@gmail.com>
2016-05-12 18:08:29 -0500

    html SongBox Changes

--- songs.js
+++ songs.js
@@ -2,21 +2,48 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
+let songArray = [];
 
+//loop thru songArray
 $.ajax({
-  url: "songs.json",
-  success: loadFunction
+    url: "songs.json"
+  }).done(function(contentsOfTheFile) {
+
+    // When you tell jQuery to read a file via the ajax method
+    // it reads the contents, and then executes whatever function
+    // that you specify here in the done() method, and passes in
+    // the contents of the file as the first argument.
+    console.log("the contents of songs.json");
+    console.log(contentsOfTheFile);
+  }).then(function(resolve, reject) {     
+  // $("#box").append( $(".content1") );
+     resolve(contentsOfTheFile);
+     reject(contentsOfTheFile);
+  });
+
 });
 
-$("#addMusic").click(function(){
-  $("#target").click();
-});
 
 
-$loadFunction = function(){
+
+
+
+
+
+// $.ajax({
+//   url: "songs.json",
+//   success: loadFunction
+// });
+
+// $("#addMusic").click(function(){
+//   $("#target").click();
+// });
+
+
+// $loadFunction = function(){
   
-}
-
+// }
+// ====================
 
 
 // let expandedSongs = [];
@@ -37,38 +64,41 @@ $loadFunction = function(){
 //   expandedSongs = songs[i].replace(/[!*@(]/g, '').replace(/>/g, '');  
 // }
 //gather song input from  user
-let createNewMusic = function(){
-  // console.log("this function works", createNewMusic);
-  let songInput = document.getElementById("Song").value;
-  let artistInput = document.getElementById("Artist").value;
-  let albumInput = document.getElementById("Album").value;
-  yourArray = [songInput, artistInput, albumInput];
-  let newSongString = toString(yourArray);
-  songs.push(newSongString);
-  console.log("songs",songs);
-  populateDOM();
-}
-function toString(arrayToConvert) {
-  console.log("arrayToConvert", arrayToConvert);
-  let songString = arrayToConvert[0] + " ";
-  songString += arrayToConvert[1] + " ";
-  songString += arrayToConvert[2];
-  return songString;
-}
-
-//convert to jquery
 
 
-addBtn.addEventListener("click", createNewMusic); 
-// populateDOM(newSongsList);
-function populateDOM() {
-  for (let i = 0; i < songs.length; i++) {
-    let songs = songs[i];
-    currentSongs.innerHTML += songs[i]; 
-    console.log("songs[i]", songs[i]);  
-    }
-};
-populateDOM(); 
-createNewMusic(songs);    
-addClickEvent();
-});
+// =======================================
+// let createNewMusic = function(){
+//   // console.log("this function works", createNewMusic);
+//   let songInput = document.getElementById("Song").value;
+//   let artistInput = document.getElementById("Artist").value;
+//   let albumInput = document.getElementById("Album").value;
+//   yourArray = [songInput, artistInput, albumInput];
+//   let newSongString = toString(yourArray);
+//   songs.push(newSongString);
+//   console.log("songs",songs);
+//   populateDOM();
+// }
+// function toString(arrayToConvert) {
+//   console.log("arrayToConvert", arrayToConvert);
+//   let songString = arrayToConvert[0] + " ";
+//   songString += arrayToConvert[1] + " ";
+//   songString += arrayToConvert[2];
+//   return songString;
+// }
+
+// //convert to jquery
+
+
+// addBtn.addEventListener("click", createNewMusic); 
+// // populateDOM(newSongsList);
+// function populateDOM() {
+//   for (let i = 0; i < songs.length; i++) {
+//     let songs = songs[i];
+//     currentSongs.innerHTML += songs[i]; 
+//     console.log("songs[i]", songs[i]);  
+//     }
+// };
+// populateDOM(); 
+// createNewMusic(songs);    
+// addClickEvent();
+// });
