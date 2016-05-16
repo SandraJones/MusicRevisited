625630820ab8482fd2478ed10a7e0cfbbfc71411
Sandy Jones <sandynashv@gmail.com>
2016-05-15 22:52:08 -0500

    utilized Firebase

--- songs.js
+++ songs.js
@@ -6,27 +6,29 @@ $(document).ready(function() {
 
     // defaultView();
     function pushTracks(data) {
-            // console.log(data);
-            data.songs.forEach(function(index, songObject){ 
-            songArray.push(data.songs[songObject]);
+            console.log(data);
+            $.each(data, function(index, songObject){ 
+            console.log("songObject", songObject);
+            songArray.push(songObject);
             });
-            // console.log("songArray", songArray);
+            console.log("songArray", songArray);
             displayTracks();
         };
 
     //loop thru songArray
     $.ajax({
-      url: "songs.json",
+      url: "https://musichistorysandy.firebaseio.com/songs/.json",
         success: pushTracks
       });
 
-     $("#moreButton").click(function() {
-            $.ajax({
-                url: "moreSongs.json",
-                success: pushTracks
-            });
-            $("#moreButton").hide();
-      });
+//commented out since using firebase
+     // $("#moreButton").click(function() {
+     //        $.ajax({
+     //            url: "moreSongs.json",
+     //            success: pushTracks
+     //        });
+     //        $("#moreButton").hide();
+     //  });
 
      //Event listeners for the add user input button
 
@@ -35,7 +37,7 @@ $(document).ready(function() {
         songArray.forEach(function(songObject) {
         console.log("songObject", songObject);
                           
-        $("#starterDom").append(`<li> ${songObject.artist}   |   ${songObject.album}   |   ${songObject.genre}</li>`)
+        $("#starterDom").append(`<li> ${songObject.artist}   |   ${songObject.album}   |   ${songObject.genre}<Button class="delete">Delete</Button></li>`)
         });
       };
 
@@ -46,23 +48,31 @@ $(document).ready(function() {
       additions.genre = $('#Song').val();
       console.log("additions", additions);
       songArray.push(additions);
+      $.ajax ({
+        url: "https://musichistorysandy.firebaseio.com/songs/.json",
+        type: "POST",
+        data: JSON.stringify(additions)
+      }).done(function(){
       displayTracks();
       $('#add-view').addClass("hidden");
-      $('#list-view').removeClass("hidden");
+      $('#list-view').removeClass("hidden");       
+      });
     });
 
     $('#addMusic').click(function(){
       $('#add-view').removeClass("hidden");
       $('#list-view').addClass("hidden");
-
     });
 
     $('#viewMusic').click(function(){
       $('#list-view').removeClass("hidden");
       $('#add-view').addClass("hidden");
     })
-
-
-
-
+    $('#starterDom').click(function(event){
+      let clickedElement = event.target;
+//what is it they clicked on... was the click on the ul, li, or delete and only want to act on the delete
+      if ($(clickedElement).hasClass('delete')) {
+         $(clickedElement).parent().remove();
+      }
+    });
  });
